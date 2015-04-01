1c5193a11a1f6debaf7a5a5dafe94aa89b2900f2
Haroenv <fingebimus@me.com>
2015-04-01 15:44:47 +0200

    pressure

--- shuttle.sh
+++ shuttle.sh
@@ -389,14 +389,21 @@ function ELSS() {
 	case $operation in
 		#air pressure
 		"*003" | "OPS003" )
-			pressure=$(($(random 146 188)/10)) #make division floating point ###
-			echo -e "cabin pressure: $pressure PSI" ###
+			min=157
+			max=177
+			for (( i = 0; i < 10; i++ )); do
+				min=$(($min+1))
+				max=$(($max-1))
+				pressure=$(($(random $min $max)/10)) #make division floating point ###
+				echo -e "cabin pressure: ${green}$pressure PSI${NC}" ###
+				sleep 1
+			done
 			exits=0
 			while [[ $exits == 0 ]]; do
 				input "${green}$operation${NC} $initialised" item
 				case $item in
-					"/001" | "ITEM001" )
-						echo -e "${green}$corr${NC}"
+					"/136" | "ITEM136" )
+						echo -e "${green}CABIN LEAK CHECK${NC}"
 						exits=1
 						;;
 					"+" | "HELP")
