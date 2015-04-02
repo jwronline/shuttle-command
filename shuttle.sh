b4d3edc3628416a3ac9d026f9de0aed408e5f11f
Haroenv <fingebimus@me.com>
2015-04-02 19:21:39 +0200

    time

--- shuttle.sh
+++ shuttle.sh
@@ -54,8 +54,8 @@ while [[ true ]]; do
 			initialised="initialised. Enter the item."
 			corr="correct code"
 			err="wrong code"
-			iteminfo="Enter ${green}ITEM${NC}, your item number  ${red}EXEC${NC}"
-			opsinfo="Enter ${green}OPS${NC}, your operation number  ${red}EXEC${NC}"
+			iteminfo="Enter ${green}ITEM${NC}, your item number ${red}EXEC${NC}"
+			opsinfo="Enter ${green}OPS${NC}, your operation number ${red}EXEC${NC}"
 			break
 			;;
 		"/002" | "ITEM002" )
@@ -127,7 +127,7 @@ function position() {
 			"-008" | "POS008" )
 				pos="PAO"
 				;;
-			"+"  | "HELP" )
+			"+" | "HELP" )
 				echo -e "$posinfo"
 				;;
 			* )
@@ -433,6 +433,16 @@ function LD() {
 
 	case $operation in
 		# pre-flight checks
+		"*101" | "*101" )
+			exits=0
+			while [[ $exits == 0 ]]; do
+				input "${green}$operation${NC} $initialised" item
+				case $item in
+					"/001" | "ITEM001" )
+						echo -e ""
+						;;
+				esac
+			done
 		"*137" | "OPS137" )
 			exits=0
 			while [[ $exits == 0 ]]; do
@@ -446,10 +456,10 @@ function LD() {
 						;;
 					# abort advisory
 					"/116" | "ITEM116" )
-							echo -e "${green}$corr${NC}"
-							echo -e "${green}abort advisory test complete${NC}" ###
-							exits=1
-							;;
+						echo -e "${green}$corr${NC}"
+						echo -e "${green}abort advisory test complete${NC}" ###
+						exits=1
+						;;
 					"+" | "HELP")
 						echo -e "$iteminfo"
 						;;
