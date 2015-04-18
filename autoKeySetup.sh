b76ddb35bfa6e9d538a44450d47b9dad5678ac2c
Haroenv <fingebimus@me.com>
2015-04-18 18:00:38 +0200

    a bit less bad setup

--- autoKeySetup.sh
+++ autoKeySetup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if [[ true ]]; then #only when it isn't installed yet
+	echo "installing autokey. Enter your password"
 	sudo apt-get install autokey
 fi
 
 if [[ false ]]; then
+	echo "moving the configuration files"
 	mv shuttle ~/.config/autokey
 fi
